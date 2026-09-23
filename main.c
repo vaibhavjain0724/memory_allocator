1a94dacb9393407b8230fb3df3f85d39508b147f
Vaibhav Jain <vaibhavjain0724@gmail.com>
2026-09-23 18:28:42 +0530

    tested my_malloc

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
 //mmap
 //asks os for a region of virtual memory
 
+#include <stdio.h>
+
+
+void *my_malloc( size_t size);
+
+int main(){
+    int *p = my_malloc(sizeof(int));
+    
+    *p = 42;
+
+    printf("%d\n", *p);
+    return 0;
+}
